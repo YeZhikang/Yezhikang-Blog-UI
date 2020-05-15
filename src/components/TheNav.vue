ebbdda9b35267bb739e44a86f6d7f6913e292108
Yezhikang <794101456@qq.com>
2020-05-15 11:50:53 +0800

    chore: 代码迁移

---
+++ src/components/TheNav.vue
@@ -0,0 +1,174 @@
+<template>
+    <div>
+        <div class="to-a">
+            <ul>
+                <li><router-link :to="{name: 'blogIndex'}"><h2><i class="el-icon-ice-cream"></i> Ye Zhikang</h2></router-link></li>
+                <div class="button-group">
+                    <li><h3><router-link :to="{name: 'me'}" :class="{navActive:isActive[0]}">About Me</router-link></h3></li>
+                    <li><h3><router-link :to="{name: 'pages'}" :class="{navActive:isActive[1]}">Articles</router-link></h3></li>
+                    <li><h3><router-link :to="{name: 'contact'}" :class="{navActive:isActive[2]}">
+                        Contact <i class="el-icon-paperclip"></i></router-link>
+                    </h3></li>
+                </div>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Nav",
+    data(){
+        return {
+            isActive: [false, false, false]
+        }
+    },
+    methods:{
+        addNavListener(){
+            let toA = document.querySelector(".to-a");
+            function func1(){
+                if(document.documentElement.scrollTop <= 20){
+                    toA.style.height = '165px' ;
+                    toA.style.boxShadow = 'none';
+                }else{
+                    toA.style.height = '58px' ;
+                    toA.style.boxShadow = '0 0 10px lightgray';
+                }
+            }
+            function func2(){
+                if(window.innerWidth <= 800){
+                    toA.style.height = '58px' ;
+                    toA.style.boxShadow = '0 0 10px lightgray';
+                    window.removeEventListener("scroll",func1,false)
+                }else{
+                    toA.style.height = '58px' ;
+                    toA.style.boxShadow = '0 0 10px lightgray';
+                    window.addEventListener('scroll',func1,false);
+                }
+            }
+            window.addEventListener('scroll',func1,false);
+            window.addEventListener('resize',func2,false);
+            if(window.innerWidth < 720){
+                window.removeEventListener('scroll',func1,false)
+            }
+        },
+        // haha2(){
+        //     let toA = document.querySelector(".to-a")
+        //     window.addEventListener('scroll',()=>{
+        //         if(document.documentElement.scrollTop <= 50){
+        //             toA.style.boxShadow = 'none';
+        //         }else{
+        //             toA.style.boxShadow = '0 0 10px lightgray';
+        //         }
+        //     },false);
+        // }
+    },
+    watch:{
+        $route: {
+            handler:function (val) {
+                if (val.name === "blogIndex") {
+                    this.isActive = [false, false, false]
+                } else if (val.name === "write") {
+                    this.isActive = [false, false, false]
+                } else if (val.name === "pages" || val.name === "articles") {
+                    this.isActive = [false, true, false]
+                } else if (val.name === "me") {
+                    this.isActive = [true, false, false]
+                } else if (val.name === "contact") {
+                    this.isActive = [false, false, true]
+                }
+            },
+            immediate:true
+        }
+    },
+    mounted() {
+        console.log(window.isMobile)
+        if(!window.isMobile){
+            this.addNavListener()
+        }
+    },
+}
+</script>
+
+<style scoped>
+    a{
+        text-decoration: none;
+        color: black;
+        transition: color 0.3s,border-top 0.15s;
+    }
+
+    a:hover{
+        color: #517598;
+        border-top: 5px solid darkcyan;
+        border-top-left-radius: 6px ;
+        border-top-right-radius: 6px  ;
+    }
+
+    .navActive{
+        color: black;
+        border-top: 5px solid darkcyan;
+        border-top-left-radius: 6px ;
+        border-top-right-radius: 6px  ;
+    }
+
+    .to-a{
+        background-color: white;
+        z-index: 999;
+        width: 100%;
+        height: 165px ;
+        box-shadow: none;
+        position: fixed;
+        transition: 0.25s ease;
+        top: 0;
+    }
+
+    /*.to-a:hover{*/
+    /*    box-shadow: none;*/
+    /*    height: 112px;*/
+    /*}*/
+
+    ul{
+        display: flex;
+        align-items: center;
+        padding: 0;
+        width: calc( 60% + (1440px - 100%)/3)  ;
+        height: 100%;
+        margin: 0 auto;
+        justify-content: space-between;
+        list-style: none;
+        transition: 0.2s ease;
+    }
+    li{
+        display: inline-block;
+    }
+
+    h3{
+        font-weight: 400;
+        font-size: 17px;
+    }
+
+    .button-group li{
+        padding-left: 20px;
+    }
+
+    @media screen and (max-width:800px ) {
+        .to-a{
+            height: 58px;
+            box-shadow: 0 0 5px lightgrey;
+        }
+        ul{
+            width: 90%;
+            justify-content: space-between;
+        }
+        h2{
+            font-size: 18px;
+        }
+        .button-group li{
+            padding-left: 10px;
+        }
+
+        h3{
+            font-size:12px;
+        }
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import './static/css/font.scss'
+import './static/css/index.scss'
+
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'https://www.yezhikang.site:8081/';
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
